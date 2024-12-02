b84b169afc244871c34f4c5e58f2405b8592a2a7
pengxiao <625498746@qq.com>
2024-12-02 18:27:32 +0800

    change huawiadd_vpninstance

--- AddHuaweiVpnInstance.py
+++ AddHuaweiVpnInstance.py
@@ -8,33 +8,33 @@ def AddHuaWeiVpnInstance():
        next_interface = line.split(",")[3]
        next_hop = line.split(",")[4]
        ipv6_next_hop = line.split(",")[5]
-       des = line.split(",")[6]
+       #des = line.split(",")[6]
        # 1 新建vpn实例
-       add_vpn_instance = """    #
-       ip vpn-instance {0}
-        ipv4-family
-        ipv6-family
-       #""".format(D20_vlan)
+       add_vpn_instance = """#
+ip vpn-instance {0}
+ ipv4-family
+ ipv6-family
+#""".format(D20_vlan)
        # 后将调度的一个出口vlan划分到某一个vpn实例里面
        add_to_d20_vlan_address ="""
-       #
-       vlan {0}
-        description {0}
-       #
-       #
-       interface Vlanif{0}
-        description {0}
-        ip binding vpn-instance {0}
-        ipv6 enable
-        ip address {1}
-        ipv6 address {2}
-       #""".format(D20_vlan, D20_address, D20_ipv6Address)
+#
+vlan {0}
+ description {0}
+#
+#
+interface Vlanif{0}
+ description {0}
+ ip binding vpn-instance {0}
+ ipv6 enable
+ ip address {1}
+ ipv6 address {2}
+#""".format(D20_vlan, D20_address, D20_ipv6Address)
        # 将此vpn实例（llddce1）缺省下一跳指向出接口网关 如出口有vpn实例则跟出口vpn实例。
        router_nex_hop = """
-       #
-       ip route-static vpn-instance {0} 0.0.0.0 0.0.0.0 {1} {2} description {4}
-       ipv6 route-static vpn-instance {0} :: 0 {1} {3} description {4}
-       #""".format(D20_vlan, next_interface, next_hop, ipv6_next_hop,des)
+#
+ip route-static vpn-instance {0} 0.0.0.0 0.0.0.0 {1} {2} description {0}_to_{1}
+ipv6 route-static vpn-instance {0} :: 0 {1} {3} description {0}_to_{1}
+#""".format(D20_vlan, next_interface, next_hop, ipv6_next_hop)
        #print("-----------------------------------------------------------------------")
        print(add_vpn_instance,add_to_d20_vlan_address,router_nex_hop)
     file.close()
@@ -90,5 +90,5 @@ ipv6 add {2}
 
 
 if __name__ == '__main__':
-    #AddHuaWeiVpnInstance()
-    AddHuaWeiPbr()
+    AddHuaWeiVpnInstance()
+    #AddHuaWeiPbr()
