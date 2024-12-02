7dc94e3f9d1b9bf9363c284c4f90e9d6e7e46732
pengxiao <625498746@qq.com>
2024-12-02 18:18:38 +0800

    add huawei PBR

--- AddHuaweiVpnInstance.py
+++ AddHuaweiVpnInstance.py
@@ -10,14 +10,13 @@ def AddHuaWeiVpnInstance():
        ipv6_next_hop = line.split(",")[5]
        des = line.split(",")[6]
        # 1 新建vpn实例
-       add_vpn_instance = """
-       #
+       add_vpn_instance = """    #
        ip vpn-instance {0}
         ipv4-family
         ipv6-family
        #""".format(D20_vlan)
        # 后将调度的一个出口vlan划分到某一个vpn实例里面
-       add_to_d20_vlan_address = """
+       add_to_d20_vlan_address ="""
        #
        vlan {0}
         description {0}
@@ -36,8 +35,60 @@ def AddHuaWeiVpnInstance():
        ip route-static vpn-instance {0} 0.0.0.0 0.0.0.0 {1} {2} description {4}
        ipv6 route-static vpn-instance {0} :: 0 {1} {3} description {4}
        #""".format(D20_vlan, next_interface, next_hop, ipv6_next_hop,des)
-       print("-----------------------------------------------------------------------")
+       #print("-----------------------------------------------------------------------")
        print(add_vpn_instance,add_to_d20_vlan_address,router_nex_hop)
     file.close()
+def AddHuaWeiPbr():
+    acl_and_classifier="""#
+acl ipv6 number 2200
+ rule  permit
+#
+acl number 2100
+ description permit ip-ALL
+ rule  permit
+#
+traffic classifier ipv4any
+ if-match acl 2100
+#
+traffic classifier ipv6any
+ if-match ipv6 acl 2200
+#"""
+    test="123"
+    with open("Address.txt", "r") as file:
+        for line in file:
+            line = line.replace("\n", "")
+            D20_vlan = line.split(",")[0]
+            D20_address = line.split(",")[1]
+            D20_ipv6Address = line.split(",")[2]
+            next_interface = line.split(",")[3]
+            next_hop = line.split(",")[4]
+            ipv6_next_hop = line.split(",")[5]
+            des = line.split(",")[6]
+            print(acl_and_classifier)
+            v4_behavior = """#
+traffic behavior v4_to_{0}
+ redirect nexthop {1}
+#""".format(next_interface,next_hop)
+            print(v4_behavior)
+            v6_behavior ="""traffic behavior v6_to_{0}
+ redirect nexthop {1}
+#""".format(next_interface,ipv6_next_hop)
+            print(v6_behavior)
+            set_Traffic_policy="""vlan {0}
+traffic-policy v4_to_{1} inbound
+traffic-policy v6_to_{1} inbound""".format(D20_vlan,next_interface)
+            print(set_Traffic_policy)
+            set_D20_address="""#
+int vlan {0}
+ip add {1}
+ipv6 enable
+ipv6 add {2}
+#""".format(D20_vlan,D20_address,D20_ipv6Address)
+            print(set_D20_address)
+
+
+
+
 if __name__ == '__main__':
-    AddHuaWeiVpnInstance()
+    #AddHuaWeiVpnInstance()
+    AddHuaWeiPbr()
